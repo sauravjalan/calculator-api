dfadd78bb4686f08e4c7d3ecc547136c0cbd70cf
sauravjalan <61405455+sauravjalan@users.noreply.github.com>
2020-10-02 13:04:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,81 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 3000
+const { Router } = require("express");
+const port = process.env.PORT || 3000;
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // your code goes here
 
 // here
+app.get("/", (req, res) => {
+  console.log("GET REQ");
+  return res.json({ message: "Hello world!" });
+});
 
+app.post("/add", (req, res) => {
+  let num1 = req.body.num1;
+  let num2 = req.body.num2;
+  if (typeof num1 === "string" || typeof num2 === "string") {
+    return res.status(200).json({ message: "Invalid data types" });
+  }
+  let sum = num1 + num2;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+  if (num1 >= 1000000 || num2 >= 1000000 || sum >= 1000000) {
+    return res.status(200).json({ message: "Overflow" });
+  }
+  res.status(200).json({ message: "the sum of given two numbers", sum: sum });
+});
+
+app.post("/sub", (req, res) => {
+  let num1 = req.body.num1;
+  let num2 = req.body.num2;
+  if (typeof num1 === "string" || typeof num2 === "string") {
+    return res.status(200).json({ message: "Invalid data types" });
+  }
+  if (num1 <= -1000000 || num2 <= -1000000) {
+    return res.status(200).json({ message: "Underflow" });
+  }
+  let sub = num1 - num2;
+  res
+    .status(200)
+    .json({ message: "the difference of given two numbers", difference: sub });
+});
+
+app.post("/multiply", (req, res) => {
+  let num1 = req.body.num1;
+  let num2 = req.body.num2;
+  if (typeof num1 === "string" || typeof num2 === "string") {
+    return res.status(200).json({ message: "Invalid data types" });
+  }
+  let mul = num1 * num2;
+  if (num1 >= 1000000 || num2 >= 1000000 || mul > 1000000) {
+    return res.status(200).json({ message: "Overflow" });
+  }
+
+  res
+    .status(200)
+    .json({ message: "The product of given numbers", result: mul });
+});
+
+app.post("/divide", (req, res) => {
+  let num1 = req.body.num1;
+  let num2 = req.body.num2;
+  if (num2 === 0) {
+    return res.status(200).json({ message: "Cannot divide by zero" });
+  }
+  let div = num1 / num2;
+  res
+    .status(200)
+    .json({ message: "The division of given numbers", result: div });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 module.exports = app;
